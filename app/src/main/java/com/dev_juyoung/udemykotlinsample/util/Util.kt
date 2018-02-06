b4f79d26c7cfb139c2713346a2f71706288e2618
dev.juyoung <dev.juyoung@gmail.com>
2018-02-06 13:41:35 +0900

    * 이미지 랜덤 표출을 위한 ext 정의.

--- app/src/main/java/com/dev_juyoung/udemykotlinsample/util/Util.kt
+++ app/src/main/java/com/dev_juyoung/udemykotlinsample/util/Util.kt
@@ -3,6 +3,7 @@ package com.dev_juyoung.udemykotlinsample.util
 import android.support.annotation.IdRes
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import java.util.*
 
 /**
  * Created by juyounglee on 2018. 2. 6..
@@ -11,3 +12,5 @@ import android.support.v7.app.AppCompatActivity
 fun AppCompatActivity.replace(@IdRes frameId: Int, fragment: Fragment, tag: String? = null) {
     supportFragmentManager.beginTransaction().replace(frameId, fragment, tag).commit()
 }
+
+fun ClosedRange<Int>.random() = Random().nextInt(endInclusive - start)
