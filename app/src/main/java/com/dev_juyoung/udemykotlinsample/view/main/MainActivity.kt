830ea1c8dfa23dedb1724853fde2b63a1bf40dff
dev.juyoung <dev.juyoung@gmail.com>
2018-02-06 11:52:25 +0900

    * 패키지 구조 변경.

--- app/src/main/java/com/dev_juyoung/udemykotlinsample/view/main/MainActivity.kt
+++ app/src/main/java/com/dev_juyoung/udemykotlinsample/view/main/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.dev_juyoung.udemykotlinsample
+package com.dev_juyoung.udemykotlinsample.view.main
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import com.dev_juyoung.udemykotlinsample.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
