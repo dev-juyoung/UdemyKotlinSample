b2180eeb46e5320a2eccf7a80a004eab6baaa911
dev.juyoung <dev.juyoung@gmail.com>
2018-02-06 13:26:13 +0900

    * HomeContract 생성 및 기본 골격 작성.

---
+++ app/src/main/java/com/dev_juyoung/udemykotlinsample/view/main/home/HomeContract.kt
@@ -0,0 +1,14 @@
+package com.dev_juyoung.udemykotlinsample.view.main.home
+
+/**
+ * Created by juyounglee on 2018. 2. 6..
+ */
+interface HomeContract {
+    interface View {
+
+    }
+
+    interface Presenter {
+        
+    }
+}
