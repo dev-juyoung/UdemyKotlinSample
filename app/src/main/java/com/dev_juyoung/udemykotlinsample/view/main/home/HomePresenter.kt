d8040918b4576958cdebd616552d1ff86c31f4f6
dev.juyoung <dev.juyoung@gmail.com>
2018-02-07 14:27:03 +0900

    * Model 분리에 따른 Presenter 코드 변경.

--- app/src/main/java/com/dev_juyoung/udemykotlinsample/view/main/home/HomePresenter.kt
+++ app/src/main/java/com/dev_juyoung/udemykotlinsample/view/main/home/HomePresenter.kt
@@ -1,21 +1,26 @@
 package com.dev_juyoung.udemykotlinsample.view.main.home
 
 import android.os.AsyncTask
+import com.dev_juyoung.udemykotlinsample.data.source.image.ImageRepository
 import com.dev_juyoung.udemykotlinsample.util.random
 
 /**
  * Created by juyounglee on 2018. 2. 6..
  */
-class HomePresenter(val view: HomeContract.View) : HomeContract.Presenter {
+class HomePresenter(
+        val view: HomeContract.View,
+        private val imageRepository: ImageRepository
+) : HomeContract.Presenter {
     override fun loadImage() {
-        ImageAsyncTask(view).execute()
+        ImageAsyncTask(view, imageRepository).execute()
     }
 
-    class ImageAsyncTask(val view: HomeContract.View) : AsyncTask<Unit, Unit, String>() {
-        override fun doInBackground(vararg params: Unit?): String {
+    class ImageAsyncTask(
+            val view: HomeContract.View,
+            val imageRepository: ImageRepository
+    ) : AsyncTask<Unit, Unit, Unit>() {
+        override fun doInBackground(vararg params: Unit?) {
             Thread.sleep(1000)
-
-            return String.format("sample_%02d", (1..10).random())
         }
 
         override fun onPreExecute() {
@@ -24,11 +29,12 @@ class HomePresenter(val view: HomeContract.View) : HomeContract.Presenter {
             view.showProgress()
         }
 
-        override fun onPostExecute(result: String?) {
+        override fun onPostExecute(result: Unit?) {
             super.onPostExecute(result)
 
             view.hideProgress()
-            result?.let {
+
+            imageRepository.loadImageFileName {
                 view.showImage(it)
             }
         }
